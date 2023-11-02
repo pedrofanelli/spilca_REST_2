8430948607d77535afdf1f76d45ecba2ef786100
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-02 20:22:31 -0300

    OpenFeign working

--- src/main/java/com/example/demo/SpilcaRest2Application.java
+++ src/main/java/com/example/demo/SpilcaRest2Application.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /*
  * This app represents the actual API we build. The app that "punches" the external API in _1
@@ -11,6 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@ComponentScan(basePackages={"controllers"})
+@EnableFeignClients(basePackages= {"openfeign"})
 public class SpilcaRest2Application {
 
 	public static void main(String[] args) {
